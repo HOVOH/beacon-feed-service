62b009f18d6894b881106b98ac6f81405a6207f1
HOVOH <71901758+HOVOH@users.noreply.github.com>
2021-07-31 22:03:04 -0400

    add validation pipe and error filter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { SerializeInterceptor } from '@hovoh/nestjs-api-lib';
+import { SerializeInterceptor, ValidationPipe } from '@hovoh/nestjs-api-lib';
+import { ApplicationErrorsFilter } from '@hovoh/nestjs-application-error';
+import { errors } from '@hovoh/nestjs-authentication-lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalInterceptors(new SerializeInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ApplicationErrorsFilter(errors.authErrorStatusMap));
   await app.listen(3002);
 }
 bootstrap();
